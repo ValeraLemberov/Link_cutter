af3f158e61bb3f24552a8739028307ad8bb8014c
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-07-15 15:52:51 +0300

    Fixed bug with authentication

--- src/main/java/com/anctest/link/cutter/config/SecurityConfig.java
+++ src/main/java/com/anctest/link/cutter/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/registration","/swagger-ui/**").permitAll()
+                .antMatchers("/new/**", "/registration").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and().httpBasic()
